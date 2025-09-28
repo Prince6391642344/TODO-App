fdd388a42d3f8189374529d7571a01ada948b801
Prince6391 <pry09876jnp@gmail.com>
2025-09-28 13:40:37 +0530

    Update firebase config

--- firebase.js
+++ firebase.js
@@ -3,16 +3,16 @@ import { initializeApp } from '@react-native-firebase/app';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
-
+import Config from 'react-native-config';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBy7XO-gPSkOl5vm39QxXl8ONMPKbv80Tg",
-  authDomain: "todo-c879d.firebaseapp.com",
-  projectId: "todo-c879d",
-  storageBucket: "todo-c879d.firebasestorage.app",
-  messagingSenderId: "841941925371",
-  appId: "1:841941925371:web:1ff894c3af1b65244e9f5e",
-  measurementId: "G-2GM6KB1BGN"
+  apiKey: Config.FIREBASE_API_KEY,
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID,
+  measurementId: Config.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase (only once)
